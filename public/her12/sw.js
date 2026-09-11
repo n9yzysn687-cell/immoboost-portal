3a0d0ca536ef68f8f08c853afabf090ca8a87865
immoboost-ai <n9yzysn687@privaterelay.appleid.com>
2026-09-11 09:59:47 +0200

    perf(HER12): cache v7 one-tap experience

--- public/her12/sw.js
+++ public/her12/sw.js
@@ -1,6 +1,6 @@
-const CACHE="her12-v9";
+const CACHE="her12-v10";
 const SHELL=[
-  "./","./index.html","./styles-v2.css","./enhancements-v3.css","./experience-v4.css","./experience-v5.css","./experience-v6.css","./program-v2.js","./runtime-v2.js","./enhancements-v3.js","./experience-v4b.js","./experience-v5.js","./experience-v6.js","./manifest.webmanifest","./icon.svg","./apple-touch-icon.png"
+  "./","./index.html","./styles-v2.css","./enhancements-v3.css","./experience-v4.css","./experience-v5.css","./experience-v6.css","./experience-v7.css","./program-v2.js","./runtime-v2.js","./enhancements-v3.js","./experience-v4b.js","./experience-v5.js","./experience-v6.js","./experience-v7.js","./manifest.webmanifest","./icon.svg","./apple-touch-icon.png"
 ];
 const MEDIA=[
   "./assets/hero.jpg","./assets/hip_thrust.jpg","./assets/rdl.jpg","./assets/bulgarian.jpg","./assets/hip_abduction.jpg",
