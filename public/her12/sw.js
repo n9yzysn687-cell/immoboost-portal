d2157c97ff4cf77d8b5329876ed96be0e630128c
immoboost-ai <n9yzysn687@privaterelay.appleid.com>
2026-09-11 12:40:17 +0200

    HER12 v13: cache adaptive coach offline

--- public/her12/sw.js
+++ public/her12/sw.js
@@ -1,6 +1,6 @@
-const CACHE="her12-v15";
+const CACHE="her12-v16";
 const SHELL=[
-  "./","./index.html","./styles-v2.css","./enhancements-v3.css","./experience-v4.css","./experience-v5.css","./experience-v6.css","./experience-v7.css","./experience-v8.css","./experience-v9.css","./experience-v10.css","./experience-v11.css","./program-v2.js","./runtime-v2.js","./enhancements-v3.js","./experience-v4b.js","./experience-v5.js","./experience-v6.js","./experience-v7.js","./experience-v8.js","./experience-v9.js","./experience-v10.js","./experience-v11.js","./manifest.webmanifest","./icon.svg","./apple-touch-icon.png"
+  "./","./index.html","./styles-v2.css","./enhancements-v3.css","./experience-v4.css","./experience-v5.css","./experience-v6.css","./experience-v7.css","./experience-v8.css","./experience-v9.css","./experience-v10.css","./experience-v11.css","./experience-v12.css","./program-v2.js","./runtime-v2.js","./enhancements-v3.js","./experience-v4b.js","./experience-v5.js","./experience-v6.js","./experience-v7.js","./experience-v8.js","./experience-v9.js","./experience-v10.js","./experience-v11.js","./experience-v12.js","./manifest.webmanifest","./icon.svg","./apple-touch-icon.png"
 ];
 const MEDIA=[
   "./assets/hero.jpg","./assets/hip_thrust.jpg","./assets/rdl.jpg","./assets/bulgarian.jpg","./assets/hip_abduction.jpg",
